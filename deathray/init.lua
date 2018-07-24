4be54d44ea5b6034daa41210f876e132faf73508
thetaepsilon <thetaepsilon@noreply.users.github.com>
2018-07-24 17:54:03 +0100

    deathray: [new code] add action invoke helper and default killer function

--- deathray/init.lua
+++ deathray/init.lua
@@ -45,6 +45,46 @@ i.ray_search = cylinder_search
 
 
 
+-- bake an action function into a search.
+-- to use this, provide a function to be called on each found objectref.
+-- *returns a function* with the same signature as the ray search,
+-- but instead of returning the objects it invokes the action on each object,
+-- passing as arguments the objectref and cached position.
+local mk_ray_action_ = function(action)
+	assert(type(action) == "function")
+	return function(...)
+		local found = cylinder_search(...)
+		for object, pos in pairs(found) do
+			action(object, pos)
+		end
+	end
+end
+i.mk_ray_action_ = mk_ray_action_
+
+
+
+-- default "death function":
+-- if it's a player, set their HP to zero on the next step;
+-- otherwise just nuke the entity via :remove()
+local kill_object = function(objectref, pos)
+	if objectref:is_player() then
+		-- :set_hp(0) apparently must be run in globalstep context to work.
+		-- thankfully the minetest.after queue is run there
+		minetest.after(0, function() objectref:set_hp(0) end)
+	else
+		objectref:remove()
+	end
+end
+i.kill_object = kill_object
+
+
+
+-- now the actual death ray!
+-- plug together the kill action with the search
+i.fire = mk_ray_action_(kill_object)
+
+
+
 -- interface export
 modtable_register("ds2.minetest.misc_game_routines.deathray", i)
 
